ed2115ab66ee4e0ee2c1a22deaae3ea715c3cc2b
uTaHanna <ishige@ualberta.ca>
2018-01-27 17:05:21 -0800

    Show the current user's name in the main page

--- src/main/java/org/si/simple_login/vaadin/views/MainView.java
+++ src/main/java/org/si/simple_login/vaadin/views/MainView.java
@@ -1,10 +1,13 @@
 package org.si.simple_login.vaadin.views;
 
 import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.CustomComponent;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
+import org.si.simple_login.repository.UserAuthenticationDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import static org.si.simple_login.vaadin.PagePaths.LOGIN;
@@ -13,8 +16,16 @@ import static org.si.simple_login.vaadin.ViewNavigator.navigator;
 @Component
 public class MainView extends CustomComponent implements View {
 
-    private Label label = new Label("You are logged in");
+    private UserAuthenticationDAO userAuthenticationDAOSQL;
+    private String currentUserName;
+    private Label label = new Label();
     private Button signOut = new Button("Sign out", e -> navigator.navigateTo(LOGIN.getPagePath()));
+
+    @Autowired
+    public void setUserAuthenticationDAOSQL(UserAuthenticationDAO userAuthenticationDAOSQL){
+
+        this.userAuthenticationDAOSQL = userAuthenticationDAOSQL;
+    }
 
     public MainView(){
 
@@ -22,5 +33,12 @@ public class MainView extends CustomComponent implements View {
         VerticalLayout mainViewLayout = new VerticalLayout(label, signOut);
 
         setCompositionRoot(mainViewLayout);
+    }
+
+    @Override
+    public void enter(ViewChangeListener.ViewChangeEvent event){
+
+        currentUserName = userAuthenticationDAOSQL.getAuthenticatedUserName();
+        label.setValue("You are logged in, " + currentUserName);
     }
 }
