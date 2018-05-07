70c106e4cb448abac0f9d32a52577df4a85e641a
uTaHanna <ishige@ualberta.ca>
2018-05-06 18:27:05 -0700

    Refine nav bar styling

--- src/main/java/org/si/simple_login/vaadin/ViewNavigator.java
+++ src/main/java/org/si/simple_login/vaadin/ViewNavigator.java
@@ -21,12 +21,13 @@ import java.util.HashSet;
 
 
 /**
- * The basic UI design of this app and so the related backend functionalities are
- * based on the Vaadin tutorials by Alejandro Duarte
- * (https://vaadin.com/blog/implementing-remember-me-with-vaadin
- * https://www.youtube.com/watch?v=-xejxaIQTO8). More generally,
+ * The basic UI design of this app and so the related backend functionalities is
+ * based on the Vaadin tutorial by Alejandro Duarte
+ * (https://vaadin.com/blog/implementing-remember-me-with-vaadin). More generally,
  * my underlying background knowledge of Vaadin apps owes to another tutorial by
  * the same author (https://vaadin.com/blog/building-a-web-ui-for-mysql-databases-in-plain-java-).
+ * For the navigation bar, in particular, the following sources were consulted:
+ * https://www.youtube.com/watch?v=-xejxaIQTO8; https://vaadin.com/forum/thread/590939
  *
  * More detailed orientation on the use of Vaadin binders was gained from the tutorial by Kirill Bulatov
  * (https://vaadin.com/blog/15624687). The page navigation is based on the official
@@ -48,7 +49,9 @@ public class ViewNavigator extends UI {
     public static SpringNavigator navigator;
     private UserAuthenticationDAO userAuthenticationDAO;
     private ComponentContainer componentContainer = new Navigator.EmptyView();
-    private MenuBar navBar = new MenuBar();
+    private MenuBar navBarLeft = new MenuBar();
+    private MenuBar navBarRight = new MenuBar();
+
     private HashSet<String> restrictedViewNames = new HashSet<>();
 
     @Autowired
@@ -60,27 +63,28 @@ public class ViewNavigator extends UI {
 
     public void init(VaadinRequest request){
 
-        // to be made into a navigation bar in the style sheet
-        navBar.addItem("Home", e -> navigator.navigateTo(MainView.NAME));
-        navBar.addItem("Profile", e -> navigator.navigateTo(ProfileView.NAME));
-        navBar.addItem("Sign Out", e -> signOut());
-        HorizontalLayout navBarLayout = new HorizontalLayout(navBar);
+        // set up the navigation bar; styling done in the scss file
+        navBarLeft.addItem("Home", e -> navigator.navigateTo(MainView.NAME));
+        navBarLeft.addItem("Profile", e -> navigator.navigateTo(ProfileView.NAME));
+        navBarRight.addItem("Sign Out", e -> signOut());
+        navBarRight.addStyleName("navBarRight");
+        HorizontalLayout navBarLayout = new HorizontalLayout(navBarLeft, navBarRight);
         navBarLayout.setStyleName("navBarLayout");
         navBarLayout.setVisible(false);
 
+        // set up the meta-layout for showing views
         componentContainer.setSizeFull();
         VerticalLayout overarchingLayout = new VerticalLayout(navBarLayout, componentContainer);
         overarchingLayout.setStyleName("overarchingLayout");
         setContent(overarchingLayout);
 
+        // initialize navigation with access control; show the nav bar for authenticated access
         navigator.init(this, componentContainer);
-
         restrictedViewNames.add(MainView.NAME);
         restrictedViewNames.add(ProfileView.NAME);
-
         navigator.addViewChangeListener(new ViewChangeListener(){
 
-            // allows only authenticated users into the main page; attempts to access by the url will fail
+            // allow only authenticated users into the main page; attempts to access by the url will fail
             @Override
             public boolean beforeViewChange(ViewChangeEvent viewChangeEvent){
 
@@ -113,6 +117,11 @@ public class ViewNavigator extends UI {
         });
     }
 
+    /**
+     * Check if requested view needs authentication, as specified in restrictedViewNames
+     * @param viewChangeEvent prompts the confirmation procedure
+     * @return true if the requested view is included in the set of restricted views; else, false
+     */
     private boolean isRestrictedView(ViewChangeListener.ViewChangeEvent viewChangeEvent){
 
         String viewNameSuffix = "view";
@@ -122,6 +131,9 @@ public class ViewNavigator extends UI {
         return restrictedViewNames.contains(viewClassName);
     }
 
+    /**
+     * Do the logout procedures, including redirection to the login page
+     */
     private void signOut(){
 
         navigator.navigateTo(LoginView.NAME);
